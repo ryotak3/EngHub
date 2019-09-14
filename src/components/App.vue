ca4a5758c5f1efd56b062b393d1eaa36fdf1bfde
Komatsu <komatsu@OwnernoMacBook-Pro.local>
2019-09-14 15:39:30 +0900

    add auth

--- src/components/App.vue
+++ src/components/App.vue
@@ -17,13 +17,26 @@
     <v-toolbar app>
       <v-toolbar-side-icon @click.stop="updateNaviBar"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>FriendlyEats</span>
+        <span>EngHub</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <template v-if="isSignedIn && userName && userPic">
+        <div class="header__user-image"><img :src='userPic' width="25" height="25"></div>
+        <p class="header__user-name">{{ userName }}</p>
+      </template>
+      <button class="header__auth-button" @click="authFunction">
+       {{ authButtonText }}
+      </button>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat><router-link to="/">Top</router-link></v-btn>
       </v-toolbar-items>
     </v-toolbar>
+    <router-view
+      :isSignedIn="isSignedIn"
+      :userName="userName"
+      :userPic="userPic"
+    >
+    </router-view>
 
     <v-content>
       <router-view />
@@ -60,6 +73,7 @@ export default {
       showModal: false,
       errorType: null,
       errorMessage: "",
+      authFunction: function () {}
     }
   },
   async created() {
@@ -89,6 +103,8 @@ export default {
         this.setError("custom", "invalid api key or not set Anonymous user on Firebase Authentication.");
       }
     }
+
+    this.onAuthStateChanged();
   },
   computed: {
   },
@@ -104,6 +120,37 @@ export default {
     openModal: function(data) {
       this.setError(data.type, data.message);
     },
+    onAuthStateChanged () {
+      firebase.auth().onAuthStateChanged( user => {
+        this.userName = user ?
+          this.getUserName() : null
+        this.userPic = user ?
+          this.getProfilePicUrl() : null
+        this.authButtonText = user ?
+          'Sign-out' : 'Sign-in'
+        this.authFunction = user ?
+          this.signOut : this.signIn
+        this.isSignedIn = user ?
+          true : false
+        
+      })
+    },
+    signIn () {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+    },
+    signOut () {
+      firebase.auth().signOut()
+    },
+    isUserSignedIn () {
+      return !!firebase.auth().currentUser || false;
+    },
+    getProfilePicUrl () {
+      return firebase.auth().currentUser.photoURL || '/static/images/profile_placeholder.png';
+    },
+    getUserName () {
+      return firebase.auth().currentUser.displayName;
+    }
   },
 }
 </script>
